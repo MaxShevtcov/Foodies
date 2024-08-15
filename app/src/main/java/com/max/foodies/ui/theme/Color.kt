b9789b339679c83d170f905f91b568030f6d480d
Max <maxschew@yandex.ru>
2024-08-15 22:25:20 +0200

    add side files

---
+++ app/src/main/java/com/max/foodies/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.max.foodies.ui.theme
+
+import androidx.compose.ui.graphics.Color
+val Orange = Color(0xF15412)
+val Dark = Color(0x333333)
+val GrayBg = Color(0xF5F5F5)
+val White = Color(0xFFFFFF)
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
---
+++ app/src/main/java/com/max/foodies/ui/theme/ColorPalette.kt
@@ -0,0 +1,10 @@
+package com.max.foodies.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+data class ColorPalette(
+    val mainColor: Color,
+    val singleTheme: Color,
+    val oppositeTheme: Color,
+    val buttonColor: Color,
+)
