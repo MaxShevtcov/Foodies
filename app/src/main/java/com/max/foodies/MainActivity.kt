a0987e163ce3482d00fc7c5333cb4d358891bd2c
Max <maxschew@yandex.ru>
2024-09-04 14:03:58 +0200

    directory refactoring

--- app/src/main/java/com/max/foodies/MainActivity.kt
+++ app/src/main/java/com/max/foodies/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.max.foodies.screens.catalogue.CatalogueScreen
+import com.max.foodies.screens.catalogueScreen.CatalogueScreen
 import com.max.foodies.ui.theme.FoodiesTheme
 
 class MainActivity : ComponentActivity() {
